0506be600d1cce12bfea91bd0620e51b69a86b31
adam_desktop <adam8310241@gmail.com>
2022-11-12 01:07:50 +0800

    backup

---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "stm32f10x.h"                  // Device header
+#include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
+#include <stdint.h>
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}

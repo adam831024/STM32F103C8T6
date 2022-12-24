c307ff3b8485d8932963155d108c077aaec7bc34
adam831024 <adam831024@yahoo.com.tw>
2022-12-24 23:36:42 +0800

    add USART loop back data

--- src/main.c
+++ src/main.c
@@ -1,9 +1,45 @@
+/******************************************************************************
+ * Copyright (C)
+ *
+ * NAME:
+ *		main.c
+ * DESCRIPTION:
+ *
+ *******************************************************************************/
+/******************************************************************************
+ * Includes
+ *******************************************************************************/
 #include "stm32f10x.h"                  // Device header
 #include "stm32f10x_gpio.h"             // Keil::Device:StdPeriph Drivers:GPIO
 #include "stm32f10x_rtc.h"              // Keil::Device:StdPeriph Drivers:RTC
 #include <stdint.h>
 #include <stdio.h>
 
+/******************************************************************************
+ * Module Preprocessor Constants
+ *******************************************************************************/
+#define CONSTANT 5
+
+/******************************************************************************
+ * Module Preprocessor Macros
+ *******************************************************************************/
+
+/******************************************************************************
+ * Module Typedefs
+ *******************************************************************************/
+
+/******************************************************************************
+ * Module Variable Definitions
+ *******************************************************************************/
+volatile char RXc;
+
+/******************************************************************************
+ * Function Prototypes
+ *******************************************************************************/
+
+/******************************************************************************
+ * Function Definitions
+ *******************************************************************************/
 /******************************************************************************
  * @brief     delay init, no use
  * @return                              void
@@ -138,6 +174,92 @@ void initGpioPin(uint32_t gpioGroup, uint16_t gpioPin, GPIOMode_TypeDef mode, GP
   }
   GPIO_Init(gpioBase, &GPIO_InitStructure);
 }
+
+void usart_init(void)
+{
+  /* Enable USART1 and GPIOA clock */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1 | RCC_APB2Periph_GPIOA, ENABLE);
+
+  /* NVIC Configuration */
+  NVIC_InitTypeDef NVIC_InitStructure;
+  /* Enable the USARTx Interrupt */
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+
+  /* Configure the GPIOs */
+  GPIO_InitTypeDef GPIO_InitStructure;
+
+  /* Configure USART1 Tx (PA.09) as alternate function push-pull */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* Configure USART1 Rx (PA.10) as input floating */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* Configure the USART1 */
+  USART_InitTypeDef USART_InitStructure;
+
+  /* USART1 configuration ------------------------------------------------------*/
+  /* USART1 configured as follow:
+    - BaudRate = 115200 baud
+    - Word Length = 8 Bits
+    - One Stop Bit
+    - No parity
+    - Hardware flow control disabled (RTS and CTS signals)
+    - Receive and transmit enabled
+    - USART Clock disabled
+    - USART CPOL: Clock is active low
+    - USART CPHA: Data is captured on the middle
+    - USART LastBit: The clock pulse of the last data bit is not output to
+      the SCLK pin
+   */
+  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+  USART_InitStructure.USART_StopBits = USART_StopBits_1;
+  USART_InitStructure.USART_Parity = USART_Parity_No;
+  USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+  USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+
+  USART_Init(USART1, &USART_InitStructure);
+
+  /* Enable USART1 */
+  USART_Cmd(USART1, ENABLE);
+
+  /* Enable the USART1 Receive interrupt: this interrupt is generated when the
+    USART1 receive data register is not empty */
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);
+}
+
+void USART1_IRQHandler(void)
+{
+  if ((USART1->SR & USART_FLAG_RXNE) != (u16)RESET)
+  {
+    RXc = USART_ReceiveData(USART1);
+
+    // Echo
+    USART_SendData(USART1, RXc);
+  }
+}
+
+void USARTSend(char *pucBuffer)
+{
+  while (*pucBuffer)
+  {
+    USART_SendData(USART1, *pucBuffer++);
+    while (USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET)
+    {
+    }
+  }
+}
+
+
 /******************************************************************************
  * @brief     main
  * @return    0
@@ -147,7 +269,10 @@ int main(void)
   /*systick timer*/
   SetSysClockTo72();
   SysTick_Init(72);
-  
+
+  // Initialize USART
+  usart_init();
+
   /*LED init*/
   initGpioPin(RCC_APB2Periph_GPIOC, GPIO_Pin_13, GPIO_Mode_Out_PP, GPIO_Speed_50MHz);
   while (1)
